f8ca37b37ed7c115e8b03a89de4fa50584a9a43a
Aleksandra Markovic <aleksandra.markovickg@gmail.com>
2022-06-20 17:06:11 +0200

    Minor typo fix

--- ITBootcamp_projekat/src/main/java/proba/Zadatak1.java
+++ ITBootcamp_projekat/src/main/java/proba/Zadatak1.java
@@ -2,6 +2,6 @@ package proba;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello worls!");
+        System.out.println("Hello world!");
     }
 }
